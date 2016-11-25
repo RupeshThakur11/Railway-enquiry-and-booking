a3d32d6a907cd9207f247942a7ac21ddb04e1b8e
rakshitkitchloo <rakshitkitchloo1996@gmail.com>
2016-11-25 23:51:33 +0530

    First commit

---
+++ src/projectrail/ProjectRail.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projectrail;
+
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import static projectrail.frontend1.trainlist;
+
+/**
+ *
+ * @author pc
+ */
+public class ProjectRail {
+    static String daa[]=new String[100];
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            new frontend1().setVisible(true);
+        
+        
+            
+                
+    }     
+    
+}
